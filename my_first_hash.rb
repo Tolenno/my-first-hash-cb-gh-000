693a0ef6a7ae37211cd1dd8923ee705ae8427738
Matt Hough <matt@hough.tv>
2017-09-29 11:36:46 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,5 +21,5 @@ def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   #your code here
   #remember to return the shipping_manifest hash
-
+  
 end
